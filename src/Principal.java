47e82754074ebbfb7f1923f68ac98d3686097f63
unknown <matheus.nguedes@senacsp.edu.br>
2026-07-19 18:22:38 -0300

    Fazer o metodos

--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,24 @@ public class Principal {
 
     public void exibeMenu(){
 
+        System.out.println("""
+                ---MENU---
+                
+                1 - Cadastrar funcionário
+                2 - Listar funcionários
+                3 - Buscar funcionário
+                4 - Dar aumento
+                5 - Demitir funcionário
+                6 - Recontratar funcionário
+                7 - Listar funcionários ativos
+                8 - Listar funcionários demitidos
+                9 - Remover funcionário
+                0 - Sair
+                
+                """);
+
+
+
     }
 
 }
