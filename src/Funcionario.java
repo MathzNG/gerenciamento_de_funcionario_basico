aad14739118562cc363fd31eb6b78861bede723c
unknown <matheus.nguedes@senacsp.edu.br>
2026-07-18 22:05:39 -0300

    Classe funcionario criada, fazer menu e metodos

---
+++ src/Funcionario.java
@@ -0,0 +1,24 @@
+public class Funcionario {
+
+    String nome;
+    String cargo;
+    double salario;
+    boolean status;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+
+    public void exibeMenu(){
+
+    }
+
+}
